d46cdc8c15f22a6d883d502ab068739183f928d2
Anton Mogilev <anton-mogilev@yandex.ru>
2022-08-18 23:53:12 +0300

    Add ViewModels

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -7,26 +7,40 @@ using System.Text;
 using ReactiveUI;
 using AppFoxScreenShotTerm.Helpers;
 
-
 namespace AppFoxScreenShotTerm.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string messageText = Params.applicationPath + Path.DirectorySeparatorChar + Params.SCREENS_FOLDER + Path.DirectorySeparatorChar + "2022_08_18_11_45_09.jpg";
-        public string MessageText
+        // public string messageText = Params.applicationPath + Path.DirectorySeparatorChar + Params.SCREENS_FOLDER + Path.DirectorySeparatorChar + "2022_08_18_11_45_09.jpg";
+        // public string MessageText
+        // {
+        //     get => messageText;
+        //     set => this.RaiseAndSetIfChanged(ref messageText, value);
+        // }
+
+        public ViewModelBase mainPanelViewModel = new ViewModelBase();
+
+        public ViewModelBase MainPanelViewModel
         {
-            get => messageText;
-            set => this.RaiseAndSetIfChanged(ref messageText, value);
-        }        
+            get => mainPanelViewModel;
+            private set => this.RaiseAndSetIfChanged(ref mainPanelViewModel, value);
+        }
+
+        public MainWindowViewModel() 
+        {
+            MainPanelViewModel = new UploadScreenShotsViewModel();            
+        }
         
-        public string imageSource = Params.SCREENS_FOLDER + Path.DirectorySeparatorChar + "2022_08_18_11_45_09.jpg";
+        // public string imageSource = Params.SCREENS_FOLDER + Path.DirectorySeparatorChar + "2022_08_18_11_45_09.jpg";
 
         // public ViewModelBase downloadScreenshotsModel;
         
         public void DownloadScreenshots()
         {
-
+            MainPanelViewModel = new DownloadScreenShotsViewModel();
         }
+
+
 
         private void ShowScreenshotsList()
         {
@@ -36,7 +50,7 @@ namespace AppFoxScreenShotTerm.ViewModels
             {
                 foreach (string screenShot in screenShots)
                 {                   
-                    MessageText += screenShot + "\r\n";
+                    // MessageText += screenShot + "\r\n";
                 }
             }
 
@@ -49,15 +63,15 @@ namespace AppFoxScreenShotTerm.ViewModels
             var captureBmp = new Bitmap(Params.screenWidth, Params.screenHeight, PixelFormat.Format32bppArgb);
             using var captureGraphic = Graphics.FromImage(captureBmp);
             captureGraphic.CopyFromScreen(0, 0, 0, 0, captureBmp.Size);
-            // try
-            // {
-            //     captureBmp.Save(imagePath, ImageFormat.Jpeg);
-            //     MessageText += imagePath + "\r\n";
-            // }
-            // catch (Exception e)
-            // {
-            //     Console.WriteLine(e.ToString());
-            // }
+            try
+            {
+                captureBmp.Save(imagePath, ImageFormat.Jpeg);
+                // MessageText += imagePath + "\r\n";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
 
             
 
